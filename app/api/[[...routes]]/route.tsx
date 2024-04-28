0918b5548a8467ea8f96a29ce4e627b427a81e5b
dylsteck <dylancsteck@gmail.com>
2024-04-28 03:51:40 -0700

    fix: rendering issue, still needs slight bit more styling and state then fully good
    assume v1 kicks out since still waiting on contract

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -1,59 +1,82 @@
-// NOTE: this is all a WIP and not stable
-// working on a solution since frame transactions don't support arbitrum
 /** @jsxImportSource frog/jsx */
 
 import fetchRound from '@/lib/fetchRound'
-import { Button, Frog, TextInput, parseEther } from 'frog'
+import { Button, Frog, TextInput } from 'frog'
 import { devtools } from 'frog/dev'
+// import { neynar } from 'frog/hubs'
 import { handle } from 'frog/next'
 import { serveStatic } from 'frog/serve-static'
 
 const app = new Frog({
   assetsPath: '/',
   basePath: '/api',
-  initialState: {
-    projectId: 0
-  }
+  initialState: { id: 0, projectId: 0 }
+  // Supply a Hub to enable frame verification.
+  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
 })
+
+// Uncomment to use Edge Runtime
+// export const runtime = 'edge'
 
 app.frame('/', async(c) => {
   const { buttonValue, status, deriveState } = c
   const url = new URL(c.req.url);
   const id = url.searchParams.get("id");
-  const round = await fetchRound(`${id}`, 42161)
 
   const state: any = deriveState((previousState: any) => {
-    if (buttonValue === 'reset') previousState.count = 0
-    if(buttonValue === "inc") previousState.count++
-    if (buttonValue === 'dec') previousState.count--
+    if(previousState.id === 0) previousState.id = id ?? 20
+    if(buttonValue === "inc") previousState.projectId++
+    if (buttonValue === 'dec') previousState.projectId--
   })
+
+  const round = await fetchRound(`${id}`, 42161)
+  //const title = round.data?.rounds[0].applications[0].project.metadata.title || `Project ${state.projectId}`
 
   return c.res({
     image: (
-      <div style={{ color: 'white', display: 'flex', flexDirection: "column", gap: 2, paddingLeft: '5%', fontSize: 60 }}>
-        <p>
-          GG20 | Round ID {id}
-        </p>
-        <p>
-          {state.id.length === 0 ? round.data?.rounds[0].roundMetadata.name : 
-          round.data?.rounds[0].applications[state.id].metadata.project.title}
-        </p>
+      <div
+        style={{
+          alignItems: 'center',
+          background:
+            status === 'response'
+              ? 'linear-gradient(to right, #432889, #17101F)'
+              : 'black',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          {state.projectId === 0 ? `View projects in ${round.data?.rounds[0].roundMetadata.name}` : `Project ${state.projectId}`}
+        </div>
       </div>
     ),
-    intents: state.id.length === 0 ? [
-      <Button.Redirect location={`https://explorer.gitcoin.co/#/round/42161/${id}`}>View Round</Button.Redirect>,
-       <Button value="inc">Explore Projects</Button>,
-    ] : state.id.length > 0 ? [
-      <Button.Redirect location={`https://explorer.gitcoin.co/#/round/42161/${id}/${state.id}`}>View Project</Button.Redirect>,
-      <Button value="inc">Next</Button>,
-      <Button value="dec">Back</Button>
+    intents: state.projectId === 0 ? [
+      <Button.Redirect location={`https://explorer.gitcoin.co/#/round/42161/${id || state.id}`}>View Round</Button.Redirect>,
+      <Button value="inc">View Projects</Button>,
     ] : [
+      <Button.Redirect location={`https://explorer.gitcoin.co/#/round/42161/${id || state.id}/${state.projectId}`}>Donate to Project</Button.Redirect>,
       <Button value="inc">Next</Button>,
       <Button value="dec">Back</Button>
     ]
   })
 })
-
 
 devtools(app, { serveStatic })
 
